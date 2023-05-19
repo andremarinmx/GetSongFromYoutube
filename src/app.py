019f8aa4db7e9f166c1e831b523a123ff11b9ab3
André Marín Alonso <63176688+andremarinmx@users.noreply.github.com>
2023-05-18 23:38:37 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,3 +6,16 @@ url = 'https://www.youtube.com/playlist?list=PLNYeagcf77NqE9w2hYJ997UABaDS1L_ge'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 #$x('//div/h3/a/text()').map(x=>x.wholeText)
+
+
+#use here in console browser
+urlLibrarySongs = 'https://music.youtube.com/library/songs'
+
+const playButtonElements = document.querySelectorAll('ytmusic-play-button-renderer');
+const songList = Array.from(playButtonElements).map(playButtonElement => {
+  const ariaLabel = playButtonElement.getAttribute('aria-label');
+  const [_, songInfo] = ariaLabel.match(/Reproducir (.+)/);
+  const [songName, artist] = songInfo.split(' - ');
+  return { songName: songName.trim(), artist: artist.trim() };
+});
+console.log(songList);
